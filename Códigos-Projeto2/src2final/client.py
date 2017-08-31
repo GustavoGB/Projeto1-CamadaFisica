82045fcc8955400de19a24071237670765379107
chends888 <lchends@gmail.com>
2017-08-31 15:12:15 -0300

    Organizando repo 2

--- Códigos-Projeto2/src2final/client.py
+++ Códigos-Projeto2/src2final/client.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:Códigos-Projeto2/src2final/client.py
 ##CLIENT
 
 from enlace import *
@@ -57,63 +56,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-=======
-##CLIENT
-
-from enlace import *
-import time
-
-# Serial Com Port
-#   para saber a sua porta, execute no terminal :
-#   python -m serial.tools.list_ports
-
-serialName = "COM5"           # Ubuntu (variacao de)
-#serialName = "/dev/tty.usbmodem1411" # Mac    (variacao de)
-#serialName = "COM3"                  # Windows(variacao de)
-
-def main():
-    # Inicializa enlace
-    com = enlace(serialName)
-
-    # Ativa comunicacao
-    com.enable()
-
-    # Endereco da imagem a ser transmitida
-    imageR = "./imgs/panda.jpg"
-
-    # Log
-    print("-------------------------")
-    print("Comunicação inicializada")
-    print("  porta : {}".format(com.fisica.name))
-    print("-------------------------")
-
-    # Carrega imagem
-    print ("Carregando imagem para transmissão :")
-    print (" - {}".format(imageR))
-    print("-------------------------")
-    txBuffer = open(imageR, 'rb').read()
-    txLen    = len(txBuffer)
-    print(txLen)
-
-    # Transmite imagem
-    print("Transmitindo .... {} bytes".format(txLen))
-    start = time.time()
-    com.sendData(txBuffer)
-
-    # espera o fim da transmissão
-    while(com.tx.getIsBussy()):
-        pass
-   
-    end = time.time()
-    tempo = (end-start)
-
-    #Encerra comunicação
-    print("-------------------------")
-    print("Comunicação encerrada")
-    print("-------------------------")
-    print("Tempo de transmissão:","{0:.2f}".format(tempo),"segundos")
-    com.disable()
-
-if __name__ == "__main__":
-    main()
->>>>>>> c03f17805949e179832dd01e4fa1870a12e10831:src2final/client.py
